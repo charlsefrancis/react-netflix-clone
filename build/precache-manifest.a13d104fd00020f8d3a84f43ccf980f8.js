724008bd46cfafbfbdf7b18bfc1f0ba74359748b
charles francis <charlsefrancis112@gmail.com>
2020-07-21 21:36:02 +0800

    Add files via upload

---
+++ build/precache-manifest.a13d104fd00020f8d3a84f43ccf980f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5190ecd13cce439de98690d23033ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb67328215f6e02ccadd",
+    "url": "/static/css/main.c792e29c.chunk.css"
+  },
+  {
+    "revision": "38f42833667c47101f70",
+    "url": "/static/js/2.00a60087.chunk.js"
+  },
+  {
+    "revision": "062222f47ec86680093a3a817014f059",
+    "url": "/static/js/2.00a60087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb67328215f6e02ccadd",
+    "url": "/static/js/main.4e62e7cb.chunk.js"
+  },
+  {
+    "revision": "fdf1b30335a5e51d8dad",
+    "url": "/static/js/runtime-main.60e15acb.js"
+  },
+  {
+    "revision": "1bdab980da4963334bffa6c62cbf8424",
+    "url": "/static/media/empty_avatar.1bdab980.jpg"
+  },
+  {
+    "revision": "42cb72f5e835b0fa0a8fb10c48c9bddd",
+    "url": "/static/media/logo.42cb72f5.png"
+  }
+]);
